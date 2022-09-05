c3909d8fb94addaee82f3aad81391c767784b21d
Norhan-Khaled <107856437+Norhan-Khaled@users.noreply.github.com>
2022-09-05 20:17:30 +0300

    done

---
+++ cw-1/cw-1.dart
@@ -0,0 +1,11 @@
+void main() {
+  var firstName = 'Norhan';
+  var lastName = 'Khaled';
+  var age = 21;
+  var hobby = "making playlists";
+
+  print("FirstName : $firstName");
+  print('Last Name : $lastName');
+
+  print("I'm $age my hobby $hobby");
+}
---
+++ cw-2/cw-2.dart
@@ -0,0 +1,27 @@
+void main() {
+ var height = 1.7;
+ var weight = 70;
+  
+ var BMI = (weight) / (height * height) ; 
+  
+  
+  if (BMI<18.6){
+    print ("underweight");
+    
+  }
+  else if (BMI<24.9 && BMI >18.6){
+    print ("normal");}
+    
+     else if (BMI >18.6)
+    print ("over weight");}
+    
+    var tempF = 133;
+    var tempC = (tempF - 32) / 1.8;
+  
+  print(tempC);
+  
+  
+  
+  
+}
+
